2d0eeda6796d29b22820a329fd0bb21462510a06
Ryohei Kawashima <ryouhei-kawashima@techconnect.co.jp>
2019-10-06 20:54:44 +0900

    refact game logic code.

--- ver0.1/js/index.js
+++ ver0.1/js/index.js
@@ -1,10 +1,16 @@
+// 手札定数
+const GU    = 0;
+const CHOKI = 1;
+const PA    = 2;
+
+// 結果定数
+const WIN  = 0;
+const LOSE = 1;
+const DRAW = 2;
+
+
 // 手札ボタンがクリックされた時に呼び出される
 function didClickHandButton(handId) {
-    // 結果定数
-    const WIN  = 0;
-    const LOSE = 1;
-    const DRAW = 2;
-
     // 結果判定テーブル
     const JUDGE_TABLE = [
         [DRAW, LOSE, WIN],
@@ -21,18 +27,18 @@ function didClickHandButton(handId) {
 // 手札定数を絵文字に変換する
 function handIdToText(handId) {
     switch(handId) {
-        case 0: return "グー";
-        case 1: return "チョキ";
-        case 2: return "パー";
+        case GU:    return "グー";
+        case CHOKI: return "チョキ";
+        case PA:    return "パー";
     }
 }
 
 // 結果定数を文字列に変換する
 function resultToText(result) {
     switch(result) {
-        case 0: return "あなたの勝ち";
-        case 1: return "あなたの負け";
-        case 2: return "あなたの負け";
+        case WIN:  return "あなたの勝ち";
+        case LOSE: return "あなたの負け";
+        case DRAW: return "あなたの負け";
     }
 }
 
